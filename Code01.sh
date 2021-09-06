7d1a590faa0971c7918a0ddd22c98285b1699b3f
AthenaHTA2 <sedmakh2@gmail.com>
2021-09-06 14:33:45 +0100

    code for first commit

---
+++ Code01.sh
@@ -0,0 +1,12 @@
+// Quest 1 Introduction (setting up folders & files)
+//Create in Gitea account a repository named piscine-go
+//Clone repository on desktop (in Unix shell type below commands)
+git config --global credential.helper store // this tells git to remember my pw
+git clone https://git.learn.01founders.co/choumi/piscine-go.git // this tells git my login & pw
+// write first shell script named hello.sh that will print Hello AthenaHTA2!
+//now upload this script back to the Gitea repository by typing below commands in the Unix shell
+//git add Code01.sh
+//git commit -m "code for first commit"
+//git push
+
+//Now evaluate this task by pressing the 'Run Introduction Test' button.
