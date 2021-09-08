8e71dab13b3ebf67b6071f57a341a6f173eae074
Helena Sedmak <sedmakh2@gmail.com>
2021-09-08 17:42:22 +0100

    function checks negative nums

--- isnegative.go
+++ isnegative.go
@@ -1,17 +1,11 @@
 package piscine
 
-func IsNegative(nb int){
+import "fmt"
 
-if nb <0 {
-
-fmt.Println("T")
-
-}
-
-else {
-
-fmt.Println("F")
-
-}
-
+func IsNegative(nb int) {
+	if nb < 0 {
+		fmt.Println("T")
+	} else {
+		fmt.Println("F")
+	}
 }
