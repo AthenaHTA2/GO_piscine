8e6696e8bfef6597a7419f10b63fb3caf52e2606
Helena Sedmak <sedmakh2@gmail.com>
2021-09-09 18:55:47 +0100

    print string lenght

---
+++ strlen.go
@@ -0,0 +1,10 @@
+package piscine
+
+func strLen(s string) int {
+	RunesInStr := []rune(s)
+	lenght := '0'
+	for _, letter := range RunesInStr {
+		lenght = lenght + letter
+		return int(lenght)
+	}
+}
