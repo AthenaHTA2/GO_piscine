00220dcc39efd67b676f2d3d7bf879c6783b3e0f
AthenaHTA2 <sedmakh2@gmail.com>
2021-09-14 09:53:48 +0100

    calculate sqrt of int

--- sqrt.go
+++ sqrt.go
@@ -1,18 +1,19 @@
 package piscine
 
 func Sqrt(nb int) int {
-	var root int
+	var root int // Anthony suggestion: this variable can be dropped
 
 	for i := 1; i <= nb; i++ {
 
 		compare := i * i
-		modulo := compare % 1
+		modulo := nb % i
 
-		if compare == nb && modulo != 0 {
+		if compare == nb && modulo == 0 {
 			root = i
-		} else {
+			return root // Anthony: can say return i instead of return root
+		} else { // Anthony explanation: this step is redundant, if make change in line 18
 			root = 0
 		}
 	}
-	return root
+	return root // Change suggested by Anthony: root 0
 }
