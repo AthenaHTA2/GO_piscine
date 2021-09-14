8941ca419b2e8161ef28e10dd9395a178fa1dca5
AthenaHTA2 <sedmakh2@gmail.com>
2021-09-14 14:00:49 +0100

    return the nth rune

---
+++ nrune.go
@@ -0,0 +1,13 @@
+package piscine
+
+func NRune(s string, n int) rune {
+	sRune := []rune(s)
+	rlength := len(sRune)
+	if n < 0 { // First error trap
+		return 0
+	} else if n > rlength { // second error trap
+		return 0
+	} else {
+		return sRune[n-1]
+	}
+}
