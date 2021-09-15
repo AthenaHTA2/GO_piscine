f0345cf837fbe9a3626534a14e9d7be0b5a8bf8a
AthenaHTA2 <sedmakh2@gmail.com>
2021-09-15 20:31:50 +0100

    Capitalize first letter of each word

---
+++ capitalize.go
@@ -0,0 +1,23 @@
+package piscine
+
+func Capitalize(s string) string {
+	Rune := []rune(s)
+
+	for _, v := range Rune {
+		if v >= 'A' || v <= 'Z' {
+			v = v + 32 // turn all into lower case
+		}
+	}
+
+	for i, v := range Rune {
+		if v >= 'a' && v <= 'z' {
+			if i-1 < 0 {
+				Rune[i] = v - 32 // Capitalize the very first character in the sentence
+			} else if Rune[i-1] >= 'a' && Rune[i-1] <= 'z' || Rune[i-1] >= 'A' && Rune[i-1] <= 'Z' || Rune[i-1] >= '0' && Rune[i-1] <= '9' {
+			} else {
+				Rune[i] = v - 32 // capitalize if previous character was not lower case alphanumeric
+			}
+		}
+	}
+	return string(Rune)
+}
