93c19f06037ae707e63d29f860ebf576148e5c47
AthenaHTA2 <sedmakh2@gmail.com>
2021-09-15 11:38:29 +0100

    search for string B inside string A and return B's positio inside A

--- index.go
+++ index.go
@@ -1,26 +1,10 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	input1 := []rune(s)
-	input2 := []rune(toFind)
-	letterToFind := input2[0]
-	length := len(input2)
-	s_length := len(input1)
-
-	if toFind == "" {
-		return 0
-	}
-	if length <= 0 {
-		return -1
-	}
-
-	for index, letter := range s {
-		if letter == letterToFind {
-			if length+index > s_length { // in this instance do nothing
-			} else {
-				if s[index:length+index] == toFind {
-					return index
-				}
+	for i := range s {
+		if len(toFind) < len(s[i:]) {
+			if string(s[i:i+len(toFind)]) == toFind {
+				return i
 			}
 		}
 	}
