026b7320e7614dfa06ae7626c7cda137ebe9428b
AthenaHTA2 <sedmakh2@gmail.com>
2021-09-19 18:59:44 +0100

    prints each element of string slice in one line

---
+++ printwordstables.go
@@ -0,0 +1,24 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintWordsTables(a []string) {
+	var rSlice []rune
+	var line []rune
+
+	for i := 0; i < len(a); i++ {
+		if i < len(a) {
+			rSlice = []rune(a[i])
+			line = append(rSlice, '\n')
+			for i, _ := range line {
+				z01.PrintRune(line[i])
+			}
+		} else {
+			rSlice = []rune(a[i])
+			line = rSlice
+			for _, c := range line {
+				z01.PrintRune(line[c])
+			}
+		}
+	}
+}
