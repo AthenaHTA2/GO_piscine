f12f75abde79a98bb16632e61deb0e79ed1cbb39
AthenaHTA2 <sedmakh2@gmail.com>
2021-09-20 19:51:43 +0100

    reads content of file

---
+++ displayfile/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("File name missing")
+		return
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+		return
+	}
+	if len(os.Args) == 2 {
+		fmt.Println("Almost there")
+		return
+	}
+}
