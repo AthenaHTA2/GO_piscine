9ac38c800fb13ad19c6ca6298c7bb46d7686dcb5
AthenaHTA2 <sedmakh2@google.com>
2021-09-23 18:23:58 +0100

    sort array of int and return median

--- abort.go
+++ abort.go
@@ -4,12 +4,15 @@ func Abort(a, b, c, d, e int) int {
 	intSlice := [5]int{a, b, c, d, e}
 	var store int
 
-	for i := 0; i <= 3; i++ {
-		if intSlice[i] > intSlice[i+1] {
-			store = intSlice[i+1]
-			intSlice[i+1] = intSlice[i]
-			intSlice[i] = store
+	for j := 0; j <= 4; j++ {
+		for i := 0; i <= 3; i++ {
+			if intSlice[i] > intSlice[i+1] {
+				store = intSlice[i+1]
+				intSlice[i+1] = intSlice[i]
+				intSlice[i] = store
+			}
 		}
 	}
+
 	return intSlice[2]
 }
